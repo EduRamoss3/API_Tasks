c46613573953b16f5628c364e49a471ee2e68bc7
Eduardo Ramos <eduardoramossilva@live.com>
2024-01-21 01:15:48 -0300

    key and string add for tasks search

--- API_Minimal_Tasks/API_Minimal_Tasks/Program.cs
+++ API_Minimal_Tasks/API_Minimal_Tasks/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 
@@ -84,7 +85,7 @@ public class Tasks
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsFinally { get; set; }
-
+    public IDictionary<string, Task>? KeyTasks { get; set; }
 }
 public class AppDbContext : DbContext
 {
